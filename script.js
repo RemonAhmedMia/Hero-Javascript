3557b8a4f6bae54e0c9fff5387effd36239d3d95
TheRemonDev <riyanahmed3536@gmail.com>
2025-07-29 18:00:49 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var name = "Rimon Mia";
+var age = 23;
+var address = "Brahmanbaria Bangladesh";
+ console.log(name);
+ console.log(age);
+ console.log(address);
+ 
+
+//  for check type of variables
+
+console.log(typeof name);
+console.log(typeof age);
