020c5798cfcf9ee7032a87623c3639c8c0dd9fe8
TheRemonDev <riyanahmed3536@gmail.com>
2025-07-29 18:49:53 +0100

    string to number

--- naming.js
+++ naming.js
@@ -4,3 +4,21 @@ var myCurrentAddress = "Dhaka Dhanmondi-05" ;
 var my_current_address = "Dhaka Dhanmondi-05";
 // *03.Pascal Case
 var MyCurrentAddress = "Dhaka Dhanmondi-05";
+
+// Foundamental of js number
+
+var orrangePrice = 20;
+// var applePrice = "20" eita string hisebe count hobe, string k number e nite gele firstInt("number") diye solved korte hobe
+var applePrice = parseInt ("20")
+ console.log(orrangePrice + applePrice);
+ 
+
+ var first = 0.1;
+ var second = 0.2;
+ var total = first + second;
+//  eitar man ashbe 0.30000000000000004 karon eita spesial case eita solve korar jonno console.log(total.toFixed(1)) ekhane (1) holo doshomik er por koto ghor boshbe oita
+ console.log(total.toFixed(1));
+//  console.log(total.toFixed(2));
+//  console.log(total.toFixed(3));
+//  console.log(total.toFixed(4));
+ 
