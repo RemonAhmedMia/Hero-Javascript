d05b0aced70408665062411ab9627b5cf4635029
TheRemonDev <riyanahmed3536@gmail.com>
2025-07-30 08:48:53 +0100

    and or condition

--- module-02.js
+++ module-02.js
@@ -4,3 +4,28 @@
  } else{
     console.log("Get Lost");
  }
+
+//  And (&&) If/Else
+
+// ekhane && diye korle 2 ta condition er jekono ekta jodi mittha hoi tahole eta properly sottho na orthat eta mitthe hobe er jonno 2 ta condition e sottho hoite hobe
+
+let salery = 25000;
+let height = 61;
+
+if (salery > 20000 && height > 67) {
+    console.log("You Are Eligable");
+} else {
+    console.log ("onno Patro Dhekte Hobe");
+}
+
+
+// Otoba( || )Condition :
+
+let saleryOne = 30001;
+let heightOne = 61;
+
+if (saleryOne > 30000 || heightOne > 67) {
+    console.log("You Are Eligable");
+} else {
+    console.log ("onno Patro Dhekte Hobe");
+}
